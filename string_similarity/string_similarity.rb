5ac88afa8d70ff67ce446a751113b5b0c5af6ce9
Deepak <deepak.kumar@sourcebits.com>
2012-01-18 01:32:06 +0530

    doie

--- string_similarity/string_similarity.rb
+++ string_similarity/string_similarity.rb
@@ -2,13 +2,12 @@
 strings = []
 count = gets.to_i
 count.times { strings<< gets.chomp }
-p `date`
 def get_similarity str
     match = 0 
     len = str.length
     sub_str = (0..(len-1)).collect{ |x| str.slice(x,len)}
     sub_str.each do |s|
-        i=0
+        i = 0
         while(s[i]==str[i] &&(i<len)) do i+=1 end
         match+=i
     end
@@ -17,4 +16,3 @@ end
 
 similarity = strings.collect { |str| get_similarity(str)  }
 similarity.each{ |sim| puts sim }
-p `date`
